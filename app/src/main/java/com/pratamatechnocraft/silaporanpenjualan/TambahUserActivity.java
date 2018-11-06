18895d21d53a8e282e5f6753caf92de8bada624a
Linda Dwi Wahyuningsih <lindafilia22@gmail.com>
2018-11-06 22:23:19 +0700

    no message

---
+++ app/src/main/java/com/pratamatechnocraft/silaporanpenjualan/TambahUserActivity.java
@@ -0,0 +1,13 @@
+package com.pratamatechnocraft.silaporanpenjualan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TambahUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tambah_user);
+    }
+}
